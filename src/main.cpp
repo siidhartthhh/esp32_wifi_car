08b6afb6551363f1f8b06cd5973ec0422a87ee25
siidhartthhh <siidhartthhh@gmail.com>
2026-09-11 20:27:40 +0530

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,28 @@ const char* password = "12345678";
 const char index_html[] PROGMEM = R"rawliteral(
  
 <!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width= device-width, intial-scale=1">
+    <title>ESP32 Car Controler</title>
+
+</head>
+<body>
+        <h1>ESP Car Remote </h1.>
+        <div  class = "slider-bar">
+         <div class = "track">
+            <label>left</label>
+             <input type = "range" min = "-200" max="200" value = "0" oninput ="sendData()" onchange ="reseLeftSlider()">
+         </div>
+         <div class = "track">
+            <label>right</label>
+             <input type = "range" min = "-200" max="200" value = "0" oninput ="sendData()" onchange ="reseRightSlider()">
+            </div>
+        </div>
+
+    
+</body>
+</html>
 
 
 
